f66e8b3b927136afdfc763b58d104dab23d758e9
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-09-26 14:00:52 +0530

    Add files via upload

---
+++ Casting/Cricketer.java
@@ -0,0 +1,20 @@
+package Casting;
+
+public class Cricketer extends Player{
+	
+  void jersey()
+  {
+	  System.out.println("Blue colour jersey for all cricketers");
+  }
+  
+  void Batting()
+  {
+	  System.out.println("he keeps batting for all time ");
+  }
+  
+  void balling()
+  {
+	  System.out.println("he keeps balling for all time ");
+  }
+
+}
