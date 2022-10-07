303077d84cc1e727c3bfd1db60b38e7b0ecf95b7
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-10-07 10:54:52 +0530

    Add files via upload

---
+++ Hands-On/Encapsulation/Encapsulation.java
@@ -0,0 +1,33 @@
+package Encapsulation;
+
+ class Encapsulation {
+
+	private int Roll_no;
+	private String name;
+	private String degree;
+	
+	//create a getters and setters method
+	
+	public int getRoll_no() {
+		return Roll_no;
+	}
+	public void setRoll_no(int roll_no) {
+		Roll_no = roll_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	
+	
+	
+	
+}
---
+++ Hands-On/Encapsulation/Encapsulation_Main.java
@@ -0,0 +1,22 @@
+package Encapsulation;
+
+public class Encapsulation_Main {
+
+	public static void main(String[] args) {
+
+		Encapsulation e1=new Encapsulation();
+		
+		//calling a setter method using object of class
+		e1.setRoll_no(20);
+		e1.setName("Jyoti");
+		e1.setDegree("TYIT");
+		
+		//calling a getter method using object of class
+		
+		System.out.println("Id is :"+e1.getRoll_no());
+		System.out.println("Name is :"+e1.getName());
+		System.out.println("Degree is :"+e1.getDegree());
+
+	}
+
+}
