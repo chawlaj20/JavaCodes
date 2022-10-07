650cec3437b93a757aa68ea3cd39e1e8eab2006d
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-10-07 11:58:10 +0530

    Update Polygon_main.java

--- Hands-On/Polygon_main.java
+++ Hands-On/Polygon_main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Polygon_main {
 
 	public static void main(String[] args) {
-
+                 
+		// get input from user
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter length:");
 		int l=sc.nextInt();
@@ -13,6 +14,7 @@ public class Polygon_main {
 		System.out.println("Enter breadth");
 		int b=sc.nextInt();
 		
+		//create object of class
 		Rectangle r1=new Rectangle();
 		r1.getArea(l, b);
 		
