93ae51d1676265a7821278e759950eb4a6c4f85f
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-10-07 12:00:07 +0530

    Update Polygon.java

--- Hands-On/Polygon.java
+++ Hands-On/Polygon.java
@@ -6,9 +6,10 @@ interface Polygon {
 	void getArea(int length,int breadth);
 
 }
-
+//implements the polygon interface
 class Rectangle implements Polygon{
 	
+	//implementation of abstract method
 	public void getArea(int length,int breadth)
 	{
 		System.out.println("The area of Rectangle is:"+(length*breadth));
