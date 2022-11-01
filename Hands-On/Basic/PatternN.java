cba21751ecbf8119280a313bd847a6d0d821dea6
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-11-01 19:11:23 +0530

    Add files via upload

---
+++ Hands-On/Basic/PatternN.java
@@ -0,0 +1,32 @@
+/*
+
+11          15        
+21 22       25 
+31   33     35 
+41      44  45 
+51          55   
+          
+
+*/
+class PatternN
+{
+ 	public static void main(String[] args)
+	{
+		for(int i=1;i<=6;i++)
+		{
+		 for(int j=1;j<=6;j++)
+			{
+			           if(i==j||j==1||j==6)
+			           System.out.print("*");
+				   System.out.print("  ");
+			
+				   
+			}
+		  	System.out.println(  );
+			
+
+		
+      		 }	
+
+	  }
+ }
