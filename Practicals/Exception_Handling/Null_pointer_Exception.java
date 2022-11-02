6384729d9fd16d8d5eaf958a4f37a2724ac2d714
Jyoti Chawla <112968683+chawlaj20@users.noreply.github.com>
2022-11-02 13:18:17 +0530

    Add files via upload

---
+++ Practicals/Exception_Handling/Null_pointer_Exception.java
@@ -0,0 +1,19 @@
+package Exception_Handling;
+
+public class Null_pointer_Exception {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+			String s=null;//null value
+			System.out.println(s.charAt(0));
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("NullPointerException: "+e.getMessage());
+		}
+
+	}
+
+}
